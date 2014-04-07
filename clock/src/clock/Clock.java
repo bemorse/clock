90685f1d06ae3426b81a1d74bfd564d96d19e786
Ben Morse <bemorse@vassar.edu>
2014-04-07 16:39:25 -0400

    initial push

---
+++ clock/src/clock/Clock.java
@@ -0,0 +1,22 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package clock;
+
+/**
+ *
+ * @author bemorse
+ */
+public class Clock {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       
+        //Draw clock
+        ClockGUI clock = new ClockGUI();
+        clock.init();
+    }
+}
